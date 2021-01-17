fca92c0321f2e0bbbfd63f00008de1d135cfddf7
lifei16 <59161085+lifei16@users.noreply.github.com>
2021-01-17 21:24:32 +0800

    Update plot_qcqp2.m

--- MATLAB_code/plot_qcqp2.m
+++ MATLAB_code/plot_qcqp2.m
@@ -1,22 +1,10 @@
-f_opt1= -1.968035633575473;
 f_opt2=-1.092297236140837;
-f_opt3= -0.995184307600784;
-f_opt4=-1.250143629451023;
 
-t1=2.54;
 t2=11.90;
 
-t3=22.14;
-t4=580.84;
-qcqp1= importdata('../IPALM/results/ALM_QCQP_outer_qcqp1_tau_10',' ');
 qcqp2= importdata('../IPALM/results/ALM_QCQP_outer_qcqp2_tau_22',' ');
-qcqp3= importdata('../IPALM/results/ALM_QCQP_outer_qcqp3_tau_31',' ');
-qcqp4= importdata('../IPALM/results/ALM_QCQP_outer_qcqp4_tau_100',' ');
 
-s1= size(qcqp1,1);
 s2= size(qcqp2,1);
-s3= size(qcqp3,1);
-s4= size(qcqp4,1);
 plot(qcqp2(1:4:s2,2),log10(max( qcqp2(1:4:s2,4), (abs(qcqp2(1:4:s2,3)- f_opt2)/f_opt2))),'-->','LineWidth',2);
 
 hold on;
